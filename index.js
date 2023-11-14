6b1744a2193926342d68f9e9ad45edb7dc933af2
pchinnas <prabhuchinnaswamy@gmail.com>
2023-11-14 10:18:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
-// get all languages
+// get all languages with GET
 app.get("/language", (req, res, next) => {
     res.json(Lang);
    });
 
-// get language by id
+// get language by id with GET
 app.get('/language/:id', (req, res) => {
     const language = Lang.find(g => g.id === parseInt(req.params.id));
     if (!language) return res.status(404).send('The language with the given ID was not found.');
     res.send(language);
 });
 
+// Update a language with POST
 app.post('/updateLanguage/:id', (req, res) => {
     console.log(req);
     const language = Lang.find(g => g.id === parseInt(req.params.id));
@@ -28,6 +29,7 @@ app.post('/updateLanguage/:id', (req, res) => {
     res.send(language);
 });
 
+// Add a language with POST
 app.post("/addLanguage", function(req, res) {
     const languagae = {
         id: Lang.length + 1,
@@ -37,6 +39,7 @@ app.post("/addLanguage", function(req, res) {
     res.send(languagae); 
   });
 
+// Delete a language with DELETE
   app.delete('/deleteLanguage/:id', (req, res) => {
     const language = Lang.find(g => g.id === parseInt(req.params.id));
     if (!language) return res.status(404).send('The language with the given ID was not found.');
